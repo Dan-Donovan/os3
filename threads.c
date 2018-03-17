a5b7d67d5587348d4dc5363b2d65ceaecf99b455
vmuser <vmuser@trustyvm.cs.virginia.edu>
2018-03-17 15:58:02 -0400

    created original files for threads.c and threads.h

---
+++ threads.c
@@ -0,0 +1,54 @@
+/*
+ * Machine Problem #1 Read-only functionality implementation for FAT16/32
+ * 
+ * CS4414 Operating Systems
+ * 
+ * Dan Donovan
+ * 
+ * 
+ * myfat.cpp - c++ file containing functionality for read and write operations for FAT Systems
+ * Dan Donovan Spring 2018 
+ * 
+ * The following code display the conceptual way I would go about implementing the various read and write functionality for FAT16/FAT32 file systems. It includes an idea on how I would have coded the problem and show my understanding for what needs to be done
+ * 
+ * 
+ * 	COMPILE: make 
+ * 	OBJECTS: myfat.o, fattester.o
+ * 	
+ * 	HEADERS: myfat.h
+ * 
+ * 
+ * 
+ * 	MODIFICATIONS:
+ * 	3/14 -- created a new version of the myfat.cpp and myfat.h files as my previous files on my virtual machine were written over. These files show the function headers and method stubs as well as conceptially what I would have done in each method
+ * 
+ * 
+ * 
+ */
+
+#include "threads.h"
+#include <stdint.h>
+#define NULL 0
+
+
+/* Global Declarations */
+
+
+
+
+
+int bigger_number(void * arg){
+   
+   
+  return 1;
+  
+}
+
+
+
+int main() {
+  
+  
+  return 0;
+}
+
---
+++ threads.h
@@ -0,0 +1,9 @@
+#ifndef __THREADS_H__
+#define __THREADS_H__
+
+extern int int bigger_number(void * arg);
+
+
+
+
+#endif
